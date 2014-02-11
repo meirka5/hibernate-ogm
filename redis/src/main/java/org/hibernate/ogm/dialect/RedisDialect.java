670e2a23d1d7276a57a70fcabc7a736e29df9292
Davide D'Alto <davide@hibernate.org>
2014-02-11 13:53:56 +0000

    fix

---
+++ redis/src/main/java/org/hibernate/ogm/dialect/RedisDialect.java
@@ -0,0 +1,145 @@
+/* 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package org.hibernate.ogm.dialect;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.hibernate.LockMode;
+import org.hibernate.dialect.lock.LockingStrategy;
+import org.hibernate.id.IntegralDataTypeHolder;
+import org.hibernate.loader.custom.CustomQuery;
+import org.hibernate.ogm.datastore.impl.EmptyTupleSnapshot;
+import org.hibernate.ogm.datastore.impl.MapHelpers;
+import org.hibernate.ogm.datastore.map.impl.MapAssociationSnapshot;
+import org.hibernate.ogm.datastore.redis.impl.RedisDatastoreProvider;
+import org.hibernate.ogm.datastore.redis.impl.RedisTupleSnapshot;
+import org.hibernate.ogm.datastore.spi.Association;
+import org.hibernate.ogm.datastore.spi.AssociationContext;
+import org.hibernate.ogm.datastore.spi.Tuple;
+import org.hibernate.ogm.datastore.spi.TupleContext;
+import org.hibernate.ogm.grid.AssociationKey;
+import org.hibernate.ogm.grid.EntityKey;
+import org.hibernate.ogm.grid.EntityKeyMetadata;
+import org.hibernate.ogm.grid.RowKey;
+import org.hibernate.ogm.massindex.batchindexing.Consumer;
+import org.hibernate.ogm.type.GridType;
+import org.hibernate.ogm.util.impl.Log;
+import org.hibernate.ogm.util.impl.LoggerFactory;
+import org.hibernate.persister.entity.Lockable;
+import org.hibernate.type.Type;
+
+/**
+ * @author Seiya Kawashima <skawashima@uchicago.edu>
+ */
+public class RedisDialect implements GridDialect {
+
+	private final RedisDatastoreProvider provider;
+	private Log log = LoggerFactory.make();
+
+	public RedisDialect(RedisDatastoreProvider provider) {
+		this.provider = provider;
+	}
+
+	@Override
+	public LockingStrategy getLockingStrategy(Lockable lockable, LockMode lockMode) {
+		// TODO Implementing this method needs help from Redis community. Once figuring out how to map lock strategies
+		// with Redis, this method will be implemented. Until that time, this method simply throws an exception.
+		throw new RuntimeException( "the lock is not supported yet." );
+	}
+
+	@Override
+	public Tuple getTuple(EntityKey key, TupleContext context) {
+		Map<String, Object> entityMap = provider.getEntityTuple( key );
+
+		if ( entityMap == null ) {
+			return null;
+		}
+
+		return new Tuple( new RedisTupleSnapshot( entityMap ) );
+	}
+
+	@Override
+	public Tuple createTuple(EntityKey key) {
+		Map<String, Object> tuple = new HashMap<String, Object>();
+		return new Tuple( new RedisTupleSnapshot( tuple ) );
+	}
+
+	@Override
+	public void updateTuple(Tuple tuple, EntityKey key) {
+		Map<String, Object> entityRecord = ( (RedisTupleSnapshot) tuple.getSnapshot() ).getMap();
+		MapHelpers.applyTupleOpsOnMap( tuple, entityRecord );
+		provider.putEntity( key, provider.getJsonHelper().convertJsonAsNeededOn( entityRecord ) );
+	}
+
+	@Override
+	public void removeTuple(EntityKey key) {
+		provider.removeEntity( key );
+	}
+
+	@Override
+	public Association getAssociation(AssociationKey key, AssociationContext context) {
+		Map<RowKey, Map<String, Object>> associationMap = provider.getAssociation( key );
+		return associationMap == null ? null : new Association( new MapAssociationSnapshot( associationMap ) );
+	}
+
+	@Override
+	public Association createAssociation(AssociationKey key, AssociationContext context) {
+		Map<RowKey, Map<String, Object>> associationMap = new HashMap<RowKey, Map<String, Object>>();
+		return new Association( new MapAssociationSnapshot( associationMap ) );
+	}
+
+	@Override
+	public void updateAssociation(Association association, AssociationKey key, AssociationContext context) {
+		MapHelpers.updateAssociation( association, key );
+		provider.putAssociation( key, ( (MapAssociationSnapshot) association.getSnapshot() ).getUnderlyingMap() );
+	}
+
+	@Override
+	public void removeAssociation(AssociationKey key, AssociationContext context) {
+		provider.removeAssociation( key );
+	}
+
+	@Override
+	public Tuple createTupleAssociation(AssociationKey associationKey, RowKey rowKey) {
+		return new Tuple( EmptyTupleSnapshot.INSTANCE );
+	}
+
+	@Override
+	public void nextValue(RowKey key, IntegralDataTypeHolder value, int increment, int initialValue) {
+		provider.setNextValue( key, value, increment, initialValue );
+	}
+
+	@Override
+	public GridType overrideType(Type type) {
+		return null;
+	}
+
+	@Override
+	public void forEachTuple(Consumer consumer, EntityKeyMetadata... entityKeyMetadatas) {
+	}
+
+	@Override
+	public Iterator<Tuple> executeBackendQuery(CustomQuery customQuery, EntityKeyMetadata[] metadatas) {
+		return null;
+	}
+
+}
---
+++ redis/src/test/java/org/hibernate/ogm/test/utils/RedisTestHelper.java
@@ -0,0 +1,83 @@
+package org.hibernate.ogm.test.utils;
+
+import java.util.Map;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.ogm.cfg.OgmConfiguration;
+import org.hibernate.ogm.datastore.redis.impl.RedisDatastoreProvider;
+import org.hibernate.ogm.datastore.spi.DatastoreProvider;
+import org.hibernate.ogm.grid.AssociationKey;
+import org.hibernate.ogm.grid.EntityKey;
+import org.hibernate.ogm.grid.RowKey;
+import org.hibernate.ogm.options.generic.document.AssociationStorageType;
+import org.hibernate.ogm.options.navigation.context.GlobalContext;
+
+public class RedisTestHelper implements TestableGridDialect {
+
+	@Override
+	public Map<String, Object> extractEntityTuple(SessionFactory sessionFactory, EntityKey key) {
+		return getEntityMap( sessionFactory ).get( key );
+	}
+
+	private static Map<EntityKey, Map<String, Object>> getEntityMap(SessionFactory sessionFactory) {
+		RedisDatastoreProvider castProvider = getProvider( sessionFactory );
+		return castProvider.getEntityMap();
+	}
+	
+	private static RedisDatastoreProvider getProvider(SessionFactory sessionFactory) {
+		DatastoreProvider provider = ( (SessionFactoryImplementor) sessionFactory ).getServiceRegistry().getService(
+				DatastoreProvider.class );
+		if ( !( RedisDatastoreProvider.class.isInstance( provider ) ) ) {
+			throw new RuntimeException( "Not testing with RedisDatastoreProvider, cannot extract underlying map." );
+		}
+		return RedisDatastoreProvider.class.cast( provider );
+	}
+	
+	private static Map<AssociationKey, Map<RowKey, Map<String,Object>>> getAssociationCache(SessionFactory sessionFactory){
+		RedisDatastoreProvider castProvider = getProvider(sessionFactory);
+		return castProvider.getAssociationsMap();
+	}
+	
+	@Override
+	public boolean backendSupportsTransactions() {
+		return false;
+	}
+
+	@Override
+	public void dropSchemaAndDatabase(SessionFactory sessionFactory) {
+		RedisDatastoreProvider castProvider = getProvider(sessionFactory);
+		castProvider.removeAll();
+	}
+
+	@Override
+	public Map<String, String> getEnvironmentProperties() {
+		return null;
+	}
+
+	@Override
+	public long getNumberOfEntities(SessionFactory sessionFactory) {
+		return getEntityMap( sessionFactory ).size();
+	}
+
+	@Override
+	public long getNumberOfAssociations(SessionFactory sessionFactory) {
+		return getAssociationCache( sessionFactory ).size();
+	}
+
+	@Override
+	public long getNumberOfAssociations(SessionFactory sessionFactory, AssociationStorageType type) {
+		return 0;
+	}
+
+	@Override
+	public long getNumberOEmbeddedCollections(SessionFactory sessionFactory) {
+		return 0;
+	}
+
+	@Override
+	public GlobalContext<?, ?> configureDatastore(OgmConfiguration configuration) {
+		return null;
+	}
+
+}
