3d97b69d5865933bc991874cef2c57555f2963c2
Davide D'Alto <davide@hibernate.org>
2014-03-20 15:23:49 +0000

    OGM-377 TupleIterator#close() does not need to throw an exception

--- mongodb/src/main/java/org/hibernate/ogm/datastore/mongodb/MongoDBDialect.java
+++ mongodb/src/main/java/org/hibernate/ogm/datastore/mongodb/MongoDBDialect.java
@@ -10,7 +10,6 @@ import static org.hibernate.ogm.datastore.mongodb.dialect.impl.MongoDBTupleSnaps
 import static org.hibernate.ogm.datastore.mongodb.dialect.impl.MongoDBTupleSnapshot.SnapshotType.UPDATE;
 import static org.hibernate.ogm.datastore.mongodb.dialect.impl.MongoHelpers.addEmptyAssociationField;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -775,7 +774,7 @@ public class MongoDBDialect implements BatchableGridDialect {
 		}
 
 		@Override
-		public void close() throws IOException {
+		public void close() {
 			cursor.close();
 		}
 	}
--- neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/Neo4jDialect.java
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/Neo4jDialect.java
@@ -8,7 +8,6 @@ package org.hibernate.ogm.datastore.neo4j;
 
 import static org.hibernate.ogm.datastore.neo4j.dialect.impl.CypherCRUD.relationshipType;
 
-import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
@@ -305,7 +304,7 @@ public class Neo4jDialect implements GridDialect {
 		}
 
 		@Override
-		public void close() throws IOException {
+		public void close() {
 			iterator.close();
 		}
 	}
