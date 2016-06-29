509d324b6751c10499d6c6d036a1829c5e6bf8a8
Davide D'Alto <davide@hibernate.org>
2016-06-29 17:22:58 +0100

    fix

--- src/main/release-scripts/push-upstream.sh
+++ src/main/release-scripts/push-upstream.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]
+RELEASE_VERSION=$1
+BRANCH=$2
+PUSH_CHANGES=${3:-false}
+
+if [ -z "$RELEASE_VERSION" ]
   then
     echo "Release version not supplied"
     exit 1
 fi
 
-RELEASE_VERSION=$1
-BRANCH=${2:-'master'}
-PUSH_CHANGES=${3:-false}
+if [ -z "$BRANCH" ]
+  then
+    echo "Branch not supplied"
+    exit 1
+fi
 
 git commit -a -m "[Jenkins release job] Preparing next development iteration"
 
