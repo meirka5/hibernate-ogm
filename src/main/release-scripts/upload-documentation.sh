b8f937e39f13d1b7ecdf7928a83bd939d8d4aee6
Davide D'Alto <davide@hibernate.org>
2016-06-29 16:56:45 +0100

    fix

--- src/main/release-scripts/upload-documentation.sh
+++ src/main/release-scripts/upload-documentation.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]
-  then
-    echo "Release version not supplied"
-fi
-
-if [ -z "$2" ]
-  then
-    echo "Family version not supplied"
-fi
-
 RELEASE_VERSION=$1
 VERSION_FAMILY=$2
+WORKSPACE=${WORKSPACE:-'.'}
+
+if [ -z "$RELEASE_VERSION" ]
+  then
+    echo "Release version not supplied"
+    exit 1
+fi
+
+if [ -z "$VERSION_FAMILY" ]
+  then
+    echo "Family version not supplied"
+    exit 1
+fi
 
 echo "##################################################"
-echo "# Hibernate OGM release $RELEASE_VERSION"
-echo "# `date`"
-echo "# Uploading documentation: Family: $VERSION_FAMILY"
+echo "# Hibernate OGM $RELEASE_VERSION"
+echo "# Uploading documentation for Family: $VERSION_FAMILY"
 echo "##################################################"
 
-unzip distribution/target/hibernate-ogm-$RELEASE_VERSION-dist.zip -d distribution/target/unpacked
-rsync -rzh --progress --delete --protocol=28 distribution/target/unpacked/dist/docs/ filemgmt.jboss.org:/docs_htdocs/hibernate/ogm/$VERSION_FAMILY
+echo "Workspace $WORKSPACE"
+unzip $WORKSPACE/distribution/target/hibernate-ogm-$RELEASE_VERSION-dist.zip -d $WORKSPACE/distribution/target/unpacked
+#rsync -rzh --progress --delete --protocol=28 $WORKSPACE/distribution/target/unpacked/dist/docs/ filemgmt.jboss.org:/docs_htdocs/hibernate/ogm/$VERSION_FAMILY
 
+echo "Documentation uploaded!"
